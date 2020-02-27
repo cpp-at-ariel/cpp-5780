f456c1de86191ae53e713e46e1a651dd3f85ce10
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-02-27 18:27:49 +0200

    hw1

--- 01-what-is-cpp/homework-a/TestExample.cpp
+++ 01-what-is-cpp/homework-a/TestExample.cpp
@@ -1,6 +1,5 @@
 /**
- * A demonstration test program for Gimatriya calculation.
- * 
+ * An example of how to write unit tests.
  * Use this as a basis to build a more complete Test.cpp file.
  * 
  * IMPORTANT: Please write more tests - the tests here are only for example and are not complete.
