ed323a54b108e182bda6c51eb6b86a9c6717923b
Erel Segal-Halevi <erelsgl@gmail.com>
2020-02-29 20:22:40 +0200

    Simplify reporters by inheritance

--- 01-what-is-cpp/homework-a/TestCounter.cpp
+++ 01-what-is-cpp/homework-a/TestCounter.cpp
@@ -4,17 +4,10 @@ using namespace doctest;
 
 const int MIN_TESTS = 20;
 
-struct ReporterCounter: public IReporter {
-    std::ostream&         stdout_stream;
-    const ContextOptions& options;
-    const TestCaseData*   test_case_data;
-
+struct ReporterCounter: public ConsoleReporter {
     ReporterCounter(const ContextOptions& input_options)
-            : stdout_stream(*input_options.cout)
-            , options(input_options) {}
+            : ConsoleReporter(input_options) {}
 
-    void report_query(const QueryData& /*in*/) override {}
-    void test_run_start() override {}
     void test_run_end(const TestRunStats& run_stats) override {
         if (run_stats.numAsserts >= MIN_TESTS) {
             std::cout << 100 << std::endl;
@@ -22,16 +15,8 @@ struct ReporterCounter: public IReporter {
             std::cout << "Please write at least " << MIN_TESTS << " tests! " <<  std::endl << 0  << std::endl;
         }
     }
-    void test_case_start(const TestCaseData& input_data) override { test_case_data = &input_data; }
-    void test_case_reenter(const TestCaseData& /*in*/) override {}
-    void test_case_end(const CurrentTestCaseStats& /*in*/) override {}
-    void test_case_exception(const TestCaseException& /*in*/) override {}
-    void subcase_start(const SubcaseSignature& /*in*/) override {}
-    void subcase_end() override { }
-    void log_assert(const AssertData& rb) override {  }
-    void log_message(const MessageData& /*in*/) override {}
-    void test_case_skipped(const TestCaseData& /*in*/) override {}
 };
+
 REGISTER_REPORTER("counter", 1, ReporterCounter);
 
 int main(int argc, char** argv) {
