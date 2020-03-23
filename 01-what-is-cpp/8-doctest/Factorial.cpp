b8fc1ffbed280e2c7cf66ff9f4c8c3bc2dbbebf5
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-03-23 19:52:19 +0200

    add example of check_throws

--- 01-what-is-cpp/8-doctest/Factorial.cpp
+++ 01-what-is-cpp/8-doctest/Factorial.cpp
@@ -1,6 +1,11 @@
 #include "Factorial.hpp"
+#include <stdexcept>
+
 
 int factorial(int number) { 
+    if (number<0)
+        throw std::invalid_argument("Argument to factorial must be non-negative");
+
     return number <= 1?
         number:
         factorial(number - 1) * number; 
--- 01-what-is-cpp/8-doctest/FactorialTest2.cpp
+++ 01-what-is-cpp/8-doctest/FactorialTest2.cpp
@@ -1,8 +1,10 @@
 #include "doctest.h"
 #include "Factorial.hpp"
+#include <stdexcept>
 
-TEST_CASE("Factorials of small numbers") {
-    CHECK(factorial(4) == 24);
+TEST_CASE("Factorials of negative numbers") { 
+    CHECK_THROWS(factorial(-1));  // check that some exception is thrown
+    CHECK_THROWS_AS(factorial(-2), std::out_of_range);  // check that a specific exception type is thrown
 }
 
 /* add more test cases here */
